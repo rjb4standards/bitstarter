5bee95413d3ec35e3f840df2b9353d5badd183be
Dick Brooks <d.brooks@ieee.org>
2013-07-09 12:30:05 +0000

    grader now checks urls

--- grader.js
+++ grader.js
@@ -24,8 +24,12 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var sys = require('util');
+var rest = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://google.com/";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -61,11 +65,17 @@ var clone = function(fn) {
     return fn.bind({});
 };
 if(require.main == module) {
+    var tmpfile = "/tmp/index.html";
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <URL>', 'URL of index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+   if (program.url){
+      rest.get(program.url).on('complete', function (result, response) {fs.writeFileSync(tmpfile, result);});
+      var checkJson = checkHtmlFile(tmpfile, program.checks);
+   } else
+       var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
